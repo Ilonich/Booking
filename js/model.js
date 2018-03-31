8969e3b09cc3682c4cd71df24e23e4d440975e22
Nikko <Niko-ikko@ya.ru>
2018-03-31 15:31:06 +0400

    modulezation

---
+++ js/model.js
@@ -0,0 +1,71 @@
+var ADS = [
+    {
+        'author': {
+            'avatar': 'img/avatars/user01.png'
+        },
+        'offer': {
+            'title': 'Большая уютная квартира',
+            'adress': 'location.x, location.y',
+            'price': dumbRandom.getRandomValue(1000000, 1000),
+            'type': 'flat',
+            'rooms': dumbRandom.getRandomNumber(5),
+            'guests': dumbRandom.getRandomNumber(5),
+            'checkin': '12:00',
+            'checkout': '12:00',
+            'features': ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"],
+            'description': '',
+            'photos': []
+        },
+
+        'location': {
+            'x': dumbRandom.getRandomValue(900, 300),
+            'y': dumbRandom.getRandomValue(500, 100)
+        }
+    },
+    {
+        'author': {
+            'avatar': 'img/avatars/user02.png'
+        },
+        'offer': {
+            'title': 'Огромный прекрасный дворец',
+            'adress': 'location.x, location.y',
+            'price': dumbRandom.getRandomValue(1000000, 1000),
+            'type': 'house',
+            'rooms': dumbRandom.getRandomNumber(5),
+            'guests': dumbRandom.getRandomNumber(3),
+            'checkin': '12:00',
+            'checkout': '13:00',
+            'features': ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"],
+            'description': '',
+            'photos': []
+        },
+
+        'location': {
+            'x': dumbRandom.getRandomValue(900, 300),
+            'y': dumbRandom.getRandomValue(500, 100)
+        }
+    },
+    {
+        'author': {
+            'avatar': 'img/avatars/user03.png'
+        },
+        'offer': {
+            'title': 'Маленькая неуютная квартира',
+            'adress': 'location.x, location.y',
+            'price': dumbRandom.getRandomValue(1000000, 1000),
+            'type': 'flat',
+            'rooms': dumbRandom.getRandomNumber(5),
+            'guests': dumbRandom.getRandomNumber(5),
+            'checkin': '13:00',
+            'checkout': '14:00',
+            'features': ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"],
+            'description': '',
+            'photos': []
+        },
+
+        'location': {
+            'x': dumbRandom.getRandomValue(900, 300),
+            'y': dumbRandom.getRandomValue(500, 100)
+        }
+    }
+];
